c954778c3b93588b8178e11210ed3907c9d4b99e
Dan Ranges <danranges@gmail.com>
2022-06-15 21:37:03 +0200

    Fixed file extensions, changed from .js to .jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
-import App from './App';
+import RouteSwitch from './RouteSwitch';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouteSwitch />
     </ChakraProvider>
   </React.StrictMode>
 );
