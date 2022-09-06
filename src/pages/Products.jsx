6bd9adfd2b10cc9bc640cd19f48ba298a53a0c20
Dan Ranges <danranges@gmail.com>
2022-09-06 20:11:08 +0200

    Add products.jsonls and render strings of products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,9 @@
-const Products = () => {
-  return <h1>Products</h1>;
-};
+import productList from "../data/products.json"
 
-export default Products;
+const Products = () => {
+  return productList.map((product, idx) => (
+    <h1 key={idx}>{JSON.stringify(product)} </h1>
+  ))
+}
+
+export default Products
