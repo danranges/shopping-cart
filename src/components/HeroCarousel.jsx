4592d91858caccb24948b52147796de552cc19ba
Dan Ranges <danranges@gmail.com>
2022-09-14 21:22:59 +0200

    Refactor HeroCarousel to own component

---
+++ src/components/HeroCarousel.jsx
@@ -0,0 +1,22 @@
+import { Carousel, Image } from "react-bootstrap"
+import useMediaQuery from "../hooks/useMediaQuery"
+import imgsCarousel from "../data/imgsCarousel.json"
+
+const HeroCarousel = () => {
+  const isDesktop = useMediaQuery("(min-width: 764px)")
+  const imgs = isDesktop
+    ? imgsCarousel.find((obj) => obj.size === "lg").imgs
+    : imgsCarousel.find((obj) => obj.size === "sm").imgs
+
+  return (
+    <Carousel ride>
+      {imgs.map((img) => (
+        <Carousel.Item>
+          <Image src={img} className="d-block w-100" />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  )
+}
+
+export default HeroCarousel
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,10 @@
-import { Container, Carousel, Image } from "react-bootstrap"
-import useMediaQuery from "../hooks/useMediaQuery"
-import imgsCarousel from "../data/imgsCarousel.json"
+import { Container } from "react-bootstrap"
+import HeroCarousel from "../components/HeroCarousel"
 
 const Home = () => {
-  const isDesktop = useMediaQuery("(min-width: 764px)")
-  const imgs = isDesktop
-    ? imgsCarousel.find((obj) => obj.size === "lg").imgs
-    : imgsCarousel.find((obj) => obj.size === "sm").imgs
-
   return (
     <Container fluid className="mx-0 px-0">
-      <Carousel ride>
-        {imgs.map((img) => (
-          <Carousel.Item>
-            <Image src={img} className="d-block w-100" />
-          </Carousel.Item>
-        ))}
-      </Carousel>
+      <HeroCarousel />
     </Container>
   )
 }
