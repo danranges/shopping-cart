b3c6cc3ce2cbe7f3ee66f7b2308bd4073d707c30
Dan Ranges <danranges@gmail.com>
2022-09-15 22:05:13 +0200

    Add home page image stack

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom"
-import { Container, Button } from "react-bootstrap"
+import { Container, Button, Stack, Image, Col } from "react-bootstrap"
 import HeroCarousel from "../components/HeroCarousel"
+import useMediaQuery from "../hooks/useMediaQuery"
 
 const Home = () => {
+  const isDesktop = useMediaQuery("(min-width: 764px)")
+  const orientation = isDesktop ? "row" : "column"
+
   return (
     <Container fluid className="mx-0 px-0">
       <HeroCarousel />
@@ -10,16 +14,36 @@ const Home = () => {
         style={{ maxWidth: "800px", height: "100%" }}
         className="d-flex flex-column justify-content-center my-4 h-100"
       >
-        <Container className="d-flex justify-content-center">
+        <Container className="d-flex justify-content-center mt-4">
           <h2>IMPROVE YOUR PERFORMANCE</h2>
         </Container>
-        <p>
+        <p className="mb-4 pb-4">
           Tenaya has been proudly handcrafting climbing shoes in Spain since
           1997, putting the needs of the climber first and designing the best
           possible footwear to help climbers achieve their goals.
         </p>
-        <h3>Disclaimer:</h3>
-        <p>
+        <Container className={`d-flex flex-${orientation} gap-1 mb-4`}>
+          <Col>
+            <Image
+              src="/imgs/home-stack/destacado01.jpg"
+              className="d-block w-100"
+            />
+          </Col>
+          <Col>
+            <Image
+              src="/imgs/home-stack/destacado02.jpg"
+              className="d-block w-100"
+            />
+          </Col>
+          <Col>
+            <Image
+              src="/imgs/home-stack/destacado03.jpg"
+              className="d-block w-100"
+            />
+          </Col>
+        </Container>
+        <h3 className="mt-4">Disclaimer:</h3>
+        <p className="mb-4">
           This storefront has no affiliation with Tenaya. All branding, assets,
           verbiage, and other intellectual property are copywritten and/or
           trademarked by Tenaya Sports, S.L. The sole purpose of this website is
